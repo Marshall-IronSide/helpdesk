58869808293b7de4dd6c04e253341f2e8c4e3de5
Marshall-IronSide <meliodas.martin@gmail.com>
2026-06-06 20:22:24 +0000

    force HTTPS in production

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TicketController;
+use Illuminate\Support\Facades\URL;
 
 Route::get('/', fn() => redirect()->route('tickets.index'));
 
